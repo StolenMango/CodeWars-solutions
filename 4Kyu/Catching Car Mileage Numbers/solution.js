facad41330f0c0bc6343817c39a2ba271b561db9
unknown <simionescuionut1@gmail.com>
2024-06-16 20:30:28 +0100

    solution

--- 4Kyu/Catching Car Mileage Numbers/solution.js
+++ 4Kyu/Catching Car Mileage Numbers/solution.js
@@ -1,4 +1,4 @@
-function isInteresting(number, awesomePhrases) {
+function shortInterest(number, awesomePhrases) {
   // only interesting if above 99
   if (number < 100) return 0;
 
@@ -22,4 +22,60 @@ function isInteresting(number, awesomePhrases) {
   }
 
   if (sameDigits === number.length) return 2;
+
+  // The digits are sequential, incrementing
+  let digitsIncrementing = 0;
+  let firstSymbol = parseInt(number[0], 10);
+  for (let symbol of number) {
+    if (symbol == firstSymbol) digitsIncrementing += 1;
+    firstSymbol += 1;
+  }
+
+  if (
+    digitsIncrementing === number.length ||
+    (digitsIncrementing === number.length - 1 &&
+      number.slice(-1) == 0 &&
+      number.slice(-2, -1) == 9)
+  )
+    return 2;
+
+  // The digits are sequential, decrementing
+  let digitsDecrementing = 0;
+  firstSymbol = parseInt(number[0], 10);
+  for (let symbol of number) {
+    if (symbol == firstSymbol) digitsDecrementing += 1;
+    firstSymbol -= 1;
+  }
+
+  if (
+    digitsDecrementing === number.length ||
+    (digitsDecrementing === number.length - 1 &&
+      number.slice(-1) == 0 &&
+      number.slice(-2, -1) == 1)
+  )
+    return 2;
+
+  // the digits are a palindrome
+  if (number === number.split("").reverse().join("")) return 2;
+
+  return -1;
+}
+
+function isInteresting(number, awesomePhrases) {
+  for (let i = 0; i <= 3; i++) {
+    let result = shortInterest(number, awesomePhrases);
+
+    if (i === 3) return 0;
+
+    if (result === 2 && i === 0) {
+      return 2;
+    } else if (result === 2 && i >= 1) {
+      return 1;
+    } else if (result === -1 || (result === 0 && i <= 2)) {
+      //do nothing
+    } else {
+      return 0;
+    }
+    number += 1;
+  }
 }
